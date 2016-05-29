9a67231da2ff39eddf0ffec09e5d41da60bb0a9f
dungvatoi12 <dungvatoi12@gmail.com>
2016-05-29 10:38:40 +0700

    [Dung] Up khung do an

---
+++ src/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var React = require('react');
+var Router = require('react-router');
+var App = require('./js/components/App')
+var ReactDom = require('react-dom');
+
+ReactDom.render(<App />, document.getElementById('app'));
